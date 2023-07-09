056fce7be0b6317433aeef6a0294625ab17c21ce
amrokh1996 <amrrrkh1996@gmail.com>
2023-07-09 22:47:11 +0300

    upload task

---
+++ client/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/*.{js,ts,tsx,jsx}",
+    "./src/**/**/*.{js,ts,tsx,jsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["light"],
+  },
+}
